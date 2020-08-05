a486299e58f413269e6c19a28d0fb26cffc9f7ff
David J. Sanderson <sanderdj90@gmail.com>
2020-08-05 14:12:37 -0600

    Add project files.

---
+++ GameEngine.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class BossEnemyShould
+    {
+        [Fact]
+        public void HaveCorrectPower()
+        {
+
+            BossEnemy sut = new BossEnemy();
+            var power = sut.SpecialAttackPower;
+            Assert.Equal(166.667, power, 3);
+
+        }
+
+    }
+}
---
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            // System Under Test (sut)
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.True(sut.IsNoob);
+        }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            // System Under Test (sut)
+            PlayerCharacter sut = new PlayerCharacter();
+
+            string fullName = $"{sut.FirstName} {sut.LastName}";
+
+            Assert.Equal(fullName, sut.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void InitialHealthIsOneHunded()
+        {
+            // System Under Test (sut)
+            PlayerCharacter sut = new PlayerCharacter();
+
+
+
+            Assert.Equal(100, sut.Health);
+        }
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            // System Under Test (sut)
+            PlayerCharacter sut = new PlayerCharacter();
+
+            int health = sut.Health;
+
+            sut.Sleep();
+
+            Assert.InRange(sut.Health, 101, 200);
+        }
+
+        [Fact]
+        public void NickNameNullWhenNew()
+        {
+            var sut = new PlayerCharacter();
+            Assert.Null(sut.Nickname);
+        }
+        [Fact]
+        public void HasStarterWeapons()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.Contains("Long Bow", sut.Weapons);
+            Assert.Contains("Short Bow", sut.Weapons);
+            Assert.Contains(sut.Weapons, weapon => weapon.Contains("Sword"));
+
+        }
+
+        [Fact]
+        public void HasNoEmptyOrNullDefaultWeapons()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.All(sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));
+
+        }
+    }
+}
